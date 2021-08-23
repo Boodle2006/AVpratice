e4b048ccfef72379912ac4ba4144903f11fce099
Ben Goldston <bgoldston.2024@saintannsny.org>
2021-08-23 11:36:16 -0400

    Initial Commit

---
+++ AVpractice/AVpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVpracticeApp.swift
+//  AVpractice
+//
+//  Created by James Goldston on 8/23/21.
+//
+
+import SwiftUI
+
+@main
+struct AVpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AVpractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AVpractice
+//
+//  Created by James Goldston on 8/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
